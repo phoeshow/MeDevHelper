38fccd2f1089b2fc5dcc09627cf9d129f782bf54
phoeshow <phoeshow.me@gmail.com>
2017-10-09 23:24:44 +0800

    add

--- client/src/renderer/App.vue
+++ client/src/renderer/App.vue
@@ -1,71 +1,56 @@
 <template>
-  <div id="app">
-    <v-app dark>
-      <v-navigation-drawer permanent clipped :mini-variant="isMiniNavigation">
-        <v-list>
-          
-          <v-list-tile tag="a" @click="toggleNavgation">
-            <v-list-tile-action>
-              <v-icon>{{isMiniNavigation ? 'chevron_right' : 'chevron_left'}}</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title></v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-
-          <v-list-tile 
-            router
-            :to="item.to"
-            :key="i"
-            v-for="(item, i) in items"
-            exact
-          >
-            <v-list-tile-action>
-              <v-icon v-html="item.icon"></v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title v-text="item.title"></v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-
-        </v-list>
-      </v-navigation-drawer>
-      <v-toolbar fixed>
-        <v-toolbar-title>{{title}}</v-toolbar-title>
-      </v-toolbar>
-      <main>
+  <v-app id="inspire" dark>
+    <v-navigation-drawer
+      clipped
+      permanent
+      v-model="drawer"
+      :mini-variant="miniNavigation"
+      app
+    >
+      <v-list dense>
+        <v-list-tile @click="miniNavigation = !miniNavigation">
+          <v-list-tile-action>
+            <v-icon>dashboard</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Dashboard</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>settings</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Settings</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app fixed clipped-left>
+      <v-toolbar-title>{{title}}</v-toolbar-title>
+    </v-toolbar>
+    <main>
+      <v-content>
         <v-container fluid fill-height>
-            <v-slide-y-transition mode="out-in">
-              <router-view></router-view>
-            </v-slide-y-transition>
+          <router-view></router-view>
         </v-container>
-      </main>
-      <v-footer :fixed="fixed">
-        <v-spacer></v-spacer>
-        <span>&copy; 2017</span>
-      </v-footer>
-    </v-app>
-  </div>
+      </v-content>
+    </main>
+    <v-footer app fixed>
+      <span>&copy; 2017</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
   export default {
-    name: 'memock-client',
     data: () => ({
-      fixed: true,
-      items: [
-        { icon: 'apps', title: 'Mock', to: '/' },
-        { icon: 'bubble_chart', title: 'Configure', to: '/inspire' }
-      ],
-      navigation: true,
-      title: 'MeMock',
-      mockserver: [], // 全局保存实例化后的服务器,
-      isMiniNavigation: true
+      title: 'Me Developer Center',
+      drawer: true,
+      miniNavigation: false
     }),
-    methods: {
-      toggleNavgation () {
-        this.isMiniNavigation = !this.isMiniNavigation
-      }
+    props: {
+      source: String
     }
   }
 </script>
